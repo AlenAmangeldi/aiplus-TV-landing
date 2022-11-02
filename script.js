fdc6a30d2d0f2302ec2f6e7a80767201662ecbb8
AlenAmangeldi <wot.benz@gmail.com>
2022-11-02 16:06:26 +0600

    new ranks animation done

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const newRanksChildren = document.getElementById("children");
 const newRanksHeader = document.getElementById("new-ranks-icon");
 const date = document.getElementById("date");
 const emptyBlock = document.getElementById("empty");
+const number = document.querySelector(":root");
 
 const rankList = [
   "Мастер (Master)",
@@ -13,25 +14,84 @@ const rankList = [
   "Легенда II (Legend II)",
   "Легенда III (Legend III)",
 ];
+let allStudents = [];
 
-function displayData(data, rank) {
+async function getData() {
+  for (let i = 0; i < 7; i++) {
+    let dates = new Date();
+    dates.setDate(dates.getDate() - i);
+    dates.setHours(-18);
+    dates.setMinutes(0);
+    dates.setSeconds(0);
+    let ISODate = dates.toISOString().substring(0, 19);
+    console.log(ISODate);
+    await fetch(
+      `https://aiplus.t8s.ru//Api/V2/GetStudents?Statuses=Занимается,Регистрация&authkey=VdqvXSXu%2Fq1DWiLefLBUihGMn7MHlvSP59HIHoHH7%2BLEtHB5dtznB6sqyJIPjH5w&extraFieldName=Дата+нового+ранга&extraFieldValue=${ISODate}`
+    )
+      .then((response) => response.json())
+      .then((data) =>
+        data.Students.forEach((student) => {
+          const studentRank = student.ExtraFields.find(
+            (el) => el.Name == "Рейтинг"
+          ).Value;
+          allStudents.push({ ...student, rank: studentRank });
+        })
+      );
+  }
+  delay();
+}
+getData();
+
+function emptyBlockSet() {
+  emptyBlock.innerHTML = `<p>Нет детей</p>`;
+}
+
+i = 0;
+async function delay() {
+  let students = allStudents.filter((student) => student.rank == rankList[i]);
+  console.log(allStudents);
+  // if (students.length == 0) {
+  //   i++;
+  //   delay();
+  // }
+  number.style.setProperty("--number", students.length);
+  number.style.setProperty(
+    "--animation-duration",
+    students.length < 6 ? "18s" : students.length * 3 + "s"
+  );
+  await displayData(students, rankList[i]);
+  setTimeout(
+    () => {
+      i++;
+      if (rankList[i] !== undefined) {
+        delay();
+      } else {
+        i = 0;
+        delay();
+      }
+    },
+    students.length < 6 ? 18500 : students.length * 3000
+  );
+}
+
+function displayData(students, rank) {
   let html = "";
   let rankIcon = "";
-  if (data.Students) {
+  if (students) {
     // console.log(data);
     rankIcon += `<img src="./assets/${rank}.png" />
         <div class="new-ranks-title">Новые ранги</div>
         <div class="new-ranks-subtitle">${rank}</div>`;
     newRanksHeader.innerHTML = rankIcon;
-    data.Students.forEach((student, index) => {
+    students.forEach((student) => {
       const studentRank = student.ExtraFields.find(
         (el) => el.Name == "Общий"
       ).Value;
       let studentImg = student.PhotoUrls
         ? "https://aiplus.t8s.ru/" + student.PhotoUrls[0]
         : "./assets/child.svg";
-      index = delaySet(index);
-      html += `<div class="child" style="--delay:${index};">
+      // index = delaySet(index);
+      html += `<div class="child" id="child">
                 <div class="child-img">
                     <img src="${studentImg}">
                 </div>
@@ -45,69 +105,7 @@ function displayData(data, rank) {
 		          </div>`;
     });
   }
+  const childClass = document.getElementById("children");
+  childClass.style["animation-duration"] = "10s";
   newRanksChildren.innerHTML = html;
 }
-
-function delaySet(index) {
-  switch (index) {
-    case 0:
-      return (index = -1);
-    case 1:
-      return (index = 0);
-    case 2:
-      return (index = 1);
-    default:
-      return (index = 2);
-  }
-}
-
-function emptyBlockSet() {
-  emptyBlock.innerHTML = `<p>Нет детей</p>`;
-}
-
-// let ranksData = [];
-// let ISODate = new Date().toISOString().substring(0, 10);
-let dates = new Date();
-dates.setDate(dates.getDate() - 1);
-let ISODate = dates.toISOString().substring(0, 10);
-console.log(ISODate);
-
-async function dataHandler() {
-  // for (let i in 7) {
-  //   await fetch(
-  //     `https://aiplus.t8s.ru//Api/V2/GetStudents?Statuses=Занимается&authkey=VdqvXSXu%2Fq1DWiLefLBUihGMn7MHlvSP59HIHoHH7%2BLEtHB5dtznB6sqyJIPjH5w&extraFieldName=Дата+нового+ранга&extraFieldValue=07.10.2022&take=10`
-  //   );
-  // }
-}
-
-// // timerHandler();
-i = 0;
-async function delay() {
-  // let date = new Date();
-  // date.setDate(date.getDate() - 1);
-  // let ISODate = date.toISOString().substring(0, 10);
-  await fetch(
-    `https://aiplus.t8s.ru//Api/V2/GetStudents?Statuses=Занимается&authkey=VdqvXSXu%2Fq1DWiLefLBUihGMn7MHlvSP59HIHoHH7%2BLEtHB5dtznB6sqyJIPjH5w&extraFieldName=Рейтинг&extraFieldValue=${rankList[i]}&extraFieldName=Дата+нового+ранга&extraFieldValue=07.10.2022&take=5`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.Students.length > 0) {
-        console.log(rankList[i]);
-        console.log(data);
-        displayData(data, rankList[i]);
-        setTimeout(() => {
-          i++;
-          if (rankList[i] !== undefined) {
-            delay();
-          } else {
-            i = 0;
-            delay();
-          }
-        }, data.Students.length * 3000);
-      } else {
-        emptyBlockSet();
-      }
-    });
-}
-
-delay();
